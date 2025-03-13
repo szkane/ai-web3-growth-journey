7aada247442ddbc96bb72431660eeeedd17ccdc5
jackiexiao <707610215@qq.com>
2025-03-13 11:36:03 +0800

    init repo from phucbm/nextra-docs-starter

---
+++ README.md
@@ -0,0 +1,24 @@
+# AI Web3 Growth Journey
+
+### Install
+```bash
+pnpm i
+```
+
+### Run the development server
+```bash
+pnpm dev
+```
+
+## License
+
+This project is licensed under the Apache License.
+
+## Thx
+This Docs initial from https://github.com/phucbm/nextra-docs-starter
+
+This starter template includes the following features:
+
+- [x] **Nextra 4**
+- [x] **Search with Pagefind**
+- [x] **Lucide Icons**
---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import {Footer, Layout, Navbar} from 'nextra-theme-docs'
+import {Banner, Head} from 'nextra/components'
+import {getPageMap} from 'nextra/page-map'
+import 'nextra-theme-docs/style.css'
+import Link from "next/link";
+
+export const metadata = {
+    // Define your metadata here
+    // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+}
+
+const banner = <Banner storageKey="some-key">AI Web3 Growth Journey Docs was created by <Link href="https://openbuild.xyz">Openbuild</Link> and <Link href="https://hackathonweekly.com">HackathonWeekly</Link></Banner>
+const navbar = (
+    <Navbar
+        logo={<img src="/images/general/logo.svg" alt="Logo" width={100} height={20}/>}
+        // ... Your additional navbar options
+    />
+)
+const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
+
+export default async function RootLayout({children}) {
+    return (
+        <html
+            // Not required, but good for SEO
+            lang="en"
+            // Required to be set
+            dir="ltr"
+            // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+            suppressHydrationWarning
+        >
+        <Head
+            // ... Your additional head options
+        >
+            <link rel="shortcut icon" href="/images/general/icon.svg"/>
+            {/* Your additional tags should be passed as `children` of `<Head>` element */}
+        </Head>
+        <body>
+        <Layout
+            banner={banner}
+            navbar={navbar}
+            pageMap={await getPageMap()}
+            docsRepositoryBase="https://github.com/phucbm/nextra-docs-starter/tree/main"
+            footer={footer}
+            // ... Your additional layout options
+        >
+            {children}
+        </Layout>
+        </body>
+        </html>
+    )
+}
