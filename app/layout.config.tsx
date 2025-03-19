b3adc5818903a86f929e2df7a9f150373a7f3456
jackiexiao <707610215@qq.com>
2025-03-19 16:05:35 +0800

    优化文档, 补充误删除内容

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -9,6 +9,7 @@ import Image from 'next/image';
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
+  githubUrl: 'https://github.com/openbuildxyz/ai-web3-growth-journey',
   nav: {
     title: (
       <>
