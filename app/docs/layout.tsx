bca342a03a04d1c847f56a91002c311acf7198e4
jackiexiao <707610215@qq.com>
2025-03-19 15:36:06 +0800

    优化文档, 清晰表明学习目标, 添加 3 月份活动信息

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -3,10 +3,24 @@ import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
 import type { Metadata } from 'next';
-
+import { GithubInfo } from 'fumadocs-ui/components/github-info';
+import type { DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
 export const metadata: Metadata = {
   title: 'AI³ Growth Journey 文档',
   description: 'AI³ Growth Journey 使用指南和开发文档',
+};
+
+const docsOptions: DocsLayoutProps = {
+  ...baseOptions,
+  tree: source.pageTree,
+  links: [
+    {
+      type: 'custom',
+      children: (
+        <GithubInfo owner="fuma-nama" repo="fumadocs" className="lg:-mx-2" />
+      ),
+    },
+  ],
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
